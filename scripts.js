751bee969be3e515e7b33417229d948f09fe06d1
Hayden Mirza <haydenjwmirza@gmail.com>
2021-01-04 12:49:09 -0500

    Initial pokemon per game request handling framework added.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,39 @@
+let generationLimits = {gen1: 151,
+                        gen2: 251,
+                        gen3: 386,
+                        gen4: 493,
+                        gen5: 649,
+                        gen6: 721,
+                        gen7: 807,
+                        gen8: 898};
+
+//Might make individual arrays for version exclusives?
+//console.log(versionExclusives['sun'][0]); <- works for indexing
+let versionExclusives = {red: [],
+                         blue: [],
+                         yellow: [],
+                         gold: [],
+                         silver: [],
+                         crystal: [],
+                         ruby: [],
+                         sapphire: [],
+                         emerald: [],
+                         diamond: [],
+                         pearl: [],
+                         platinum: [],
+                         black: [],
+                         white: [],
+                         black2: [],
+                         white2: [],
+                         x: [],
+                         y: [],
+                         sun: ["test"],
+                         moon: [],
+                         ultraSun: [],
+                         ultraMoon: [],
+                         sword: [],
+                         shield: []};
+
 function getGame(){
     console.log(document.getElementById('gameList').value);
 }
