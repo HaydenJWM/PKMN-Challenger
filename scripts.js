177e697a25846f4a340a6ae9d08f051ee725e692
Hayden Mirza <haydenjwmirza@gmail.com>
2021-01-03 23:42:58 -0500

    Reorganized frontend.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,13 @@
 function getGame(){
     console.log(document.getElementById('gameList').value);
 }
+
+function genTeam(){
+    //Placeholder
+    document.getElementById('pic1').src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png";
+    document.getElementById('pic2').src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png";
+    document.getElementById('pic3').src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png";
+    document.getElementById('pic4').src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png";
+    document.getElementById('pic5').src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png";
+    document.getElementById('pic6').src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png";
+}
